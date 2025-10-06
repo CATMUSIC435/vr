7f8bfa593e17e37df5427c5fbb47614351b567bf
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-10-06 17:37:39 +0700

    init

--- src/constants/service.jsx
+++ src/constants/service.jsx
@@ -1,6 +1,7 @@
-export const services = ["/at-t3.jpg",
-    "/at-t3.jpg",
-    "/at-t3.jpg",
-    "/at-t3.jpg",
-    "/at-t3.jpg"
+export const services = [
+    "/methods/at-t3.jpg",
+    "/methods/pt-1.jpg",
+    "/methods/pt-2.jpg",
+    "/methods/pt-3.jpg",
+    "/methods/pt-4.jpg",
 ];
